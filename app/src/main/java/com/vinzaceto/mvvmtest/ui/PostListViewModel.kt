86486d4b8078ab9403fa7a80e9d4399945e136e2
vincenzo aceto <vincenzo.aceto-apple@unina.it>
2019-02-19 16:30:13 +0100

    PostListViewModel

--- app/src/main/java/com/vinzaceto/mvvmtest/ui/PostListViewModel.kt
+++ app/src/main/java/com/vinzaceto/mvvmtest/ui/PostListViewModel.kt
@@ -2,6 +2,9 @@ package com.vinzaceto.mvvmtest.ui
 
 import com.vinzaceto.mvvmtest.base.BaseViewModel
 import com.vinzaceto.mvvmtest.network.PostApi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 /**
@@ -15,4 +18,38 @@ class PostListViewModel: BaseViewModel() {
 
     @Inject
     lateinit var postApi: PostApi
+
+    private lateinit var subscription: Disposable
+
+    init{
+        loadPosts()
+    }
+
+    private fun loadPosts() {
+        subscription = postApi.getPosts()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe{ onRetrievePostListStart() }
+            .doOnTerminate{ onRetrivePostListFinish() }
+            .subscribe(
+                { onRetrivePostListSuccess() },
+                { onRetreivePostListError() }
+            )
+    }
+
+    private fun onRetrievePostListStart(){
+
+    }
+
+    private fun onRetrivePostListFinish(){
+
+    }
+
+    private fun onRetrivePostListSuccess(){
+
+    }
+
+    private fun onRetreivePostListError(){
+
+    }
 }
